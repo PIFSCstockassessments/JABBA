19b746e40bad5a89692ef24661c747cc03ba285e
jsyslo <john.syslo@noaa.gov>
2023-05-12 09:53:07 -1000

    attempt ot fix q inits

--- R/fit_jabba.R
+++ R/fit_jabba.R
@@ -95,7 +95,7 @@ fit_jabba = function(jbinput,
   # Initial starting values (new Eq)
   if(init.values==FALSE){
     inits = function(){list(K= rlnorm(1,log(jbd$K.pr[1])-0.5*0.3^2,0.3),r = rlnorm(1,log(jbd$r.pr[1]),jbd$r.pr[2]) ,
-                            q = pmin(pmax(qbound[1]*1.05,runif(jbd$nq,min(jbd$I,na.rm=T)/max(jbd$TC,na.rm=T),mean(jbd$I,na.rm=T)/max(jbd$TC,na.rm=T))),qbound[2]*0.95)
+                            q = c(pmin(pmax(qbound[1]*1.05,runif(jbd$(nq-1),min(jbd$I,na.rm=T)/max(jbd$TC,na.rm=T),mean(jbd$I,na.rm=T)/max(jbd$TC,na.rm=T))),qbound[2]*0.95),NA), #JS added nq-1 and NA for second q
                             ,psi=rbeta(1,ab[1],ab[2]),isigma2.est=runif(1,20,100), itau2=runif(jbd$nvar,80,200))}
   }else {
     
