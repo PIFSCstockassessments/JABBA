355af460d7f52da93bc51ea5b9af724f91635196
MOshima-PIFSC <megumi.oshima@noaa.gov>
2023-05-24 13:20:44 -1000

    fix test

--- tests/testthat/test-test-fit_jabba.R
+++ tests/testthat/test-test-fit_jabba.R
@@ -6,7 +6,7 @@ test_that("fit_jabba works", {
                            cpue = test.cpue,
                            se = test.se, verbose = FALSE)
   #fit jabba model
-  fit_test <- fit_jabba(jabba_obj_abs, ni = 100000, nt = 5, nb = 7000, nc = 2, save.csvs = TRUE, output.dir = test_path("Fixtures"), jagsdir = test_path("Fixtures"))
+  fit_test <- fit_jabba(jabba_obj, ni = 100000, nt = 5, nb = 7000, nc = 2, save.csvs = TRUE, output.dir = test_path("Fixtures"), jagsdir = test_path("Fixtures"))
   expect_equal(fit_test$pars$Median[2], 0.3838237) #r
   expect_equal(fit_test$pars$Median[5], 0.88276642020) #psi
   expect_equal(fit_test$pars$Median[9], 2.00000000000) #m
