ac949faaed484ccebd00fb81dfeb37f593575123
jsyslo <john.syslo@noaa.gov>
2023-08-05 12:12:41 -1000

    isigma2

--- R/jabba2jags.R
+++ R/jabba2jags.R
@@ -253,7 +253,7 @@ cat("
 
     phi ~ dnorm(0,1.0E-4) I(-1,1)   #serial autocorrelation coefficient  
     log.resid.0 ~ dnorm(0,tau.red) T(-5,5)
-    tau.red <- isigma * (1-phi*phi)  #this is precision of total biomass variation that includes white noise and correlated error
+    tau.red <- isigma2 * (1-phi*phi)  #this is precision of total biomass variation that includes white noise and correlated error
     sigma.red <- 1 / sqrt(tau.red)
     
     for (t in 2:N)
